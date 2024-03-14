e50ee5aa0c21cce8f9f5d9084e1641ac41ab803d
fakhruddeenNha <fakhruddeen.mca@gmail.com>
2024-03-14 10:55:38 +0530

    space remove

--- hrms-employer-service/src/main/java/com/cotodel/hrms/auth/server/model/EmployeeProfileEntity.java
+++ hrms-employer-service/src/main/java/com/cotodel/hrms/auth/server/model/EmployeeProfileEntity.java
@@ -2,25 +2,16 @@ package com.cotodel.hrms.auth.server.model;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.Date;
-import java.util.List;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
