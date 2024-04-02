60540b7a5f0a0f4a6a391f31a600c6df1309277d
fakhruddeenNha <fakhruddeen.mca@gmail.com>
2024-04-02 10:26:08 +0530

    properties

---
+++ hrms-employer-service/src/main/java/com/cotodel/hrms/auth/server/properties/ApplicationConstantConfig.java
@@ -0,0 +1,16 @@
+package com.cotodel.hrms.auth.server.properties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Configuration
+@PropertySource({"classpath:application.properties"})
+public class ApplicationConstantConfig {
+	
+	@Value("${user.service.add.url}")
+	public String userServiceAddUrl;
+	
+}
