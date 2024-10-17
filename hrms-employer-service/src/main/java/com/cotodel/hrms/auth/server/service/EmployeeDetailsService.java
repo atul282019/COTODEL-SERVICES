811918d330d3b289b298ceb8be692db35921d27a
Fakhruddeen <fakhruddeen.mca@gmail.com>
2024-10-17 17:34:21 +0530

    code deleted

--- hrms-employer-service/src/main/java/com/cotodel/hrms/auth/server/service/EmployeeDetailsService.java
+++ hrms-employer-service/src/main/java/com/cotodel/hrms/auth/server/service/EmployeeDetailsService.java
@@ -1,23 +1,10 @@
 package com.cotodel.hrms.auth.server.service;
 
-import java.util.List;
-
 import com.cotodel.hrms.auth.server.dto.EmployeeDetailsRequest;
-import com.cotodel.hrms.auth.server.model.EmployeeDetailsEntity;
 
 public interface EmployeeDetailsService {
 	
 	public EmployeeDetailsRequest  saveEmpDetails(EmployeeDetailsRequest request);	
-	//public List<EmployeeDetailsEntity>  getEmpDetailsList(Long empid);	
-	//public EmployeeFamilyDetailsRequest  saveEmpFamilyDetails(EmployeeFamilyDetailsRequest request);
-	//public List<EmployeeFamilyDetailEntity>  getEmpFamilyDetailsList(Long empid);
-	//public QualificationRequest  saveQualification(QualificationRequest request);
-	//public List<QualificationEntity>  getQualificationList(Long empid,Long employerid);
-	//public ExperienceRequest  saveExperience(ExperienceRequest request);
-	//public List<ExperienceEntity>  getExperienceList(Long empid);
-	//public CertificateRequest  saveCertificate(CertificateRequest request);
-	//public List<CertificateEntity>  getCertificateList(Long empid);
-	//public ProjectRequest  saveProject(ProjectRequest request);
-	//public List<ProjectEntity>  getProjectList(Long empid);
+	
 	
 }
