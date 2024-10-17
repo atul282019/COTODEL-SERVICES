74a9bbe12d5acc15150ff3287a4d9b0256284e44
Fakhruddeen <fakhruddeen.mca@gmail.com>
2024-10-17 15:02:49 +0530

    comment

--- hrms-employer-service/src/main/java/com/cotodel/hrms/auth/server/service/impl/EmployeeOnboardingServiceImpl.java
+++ hrms-employer-service/src/main/java/com/cotodel/hrms/auth/server/service/impl/EmployeeOnboardingServiceImpl.java
@@ -3,8 +3,6 @@ package com.cotodel.hrms.auth.server.service.impl;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Column;
-
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
