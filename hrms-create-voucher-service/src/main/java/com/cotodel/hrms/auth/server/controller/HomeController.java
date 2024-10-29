d42bcb166c589ed1fcb3147926d99a6adddcec66
Fakhruddeen <fakhruddeen.mca@gmail.com>
2024-10-29 10:55:31 +0530

    sout

--- hrms-create-voucher-service/src/main/java/com/cotodel/hrms/auth/server/controller/HomeController.java
+++ hrms-create-voucher-service/src/main/java/com/cotodel/hrms/auth/server/controller/HomeController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home() {
+    	System.out.println("homepage");
         return "Welcome to the homepage!";
     }
+    
 }
