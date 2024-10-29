73b8214c89c514190f99bc1988cf7a215af94210
Fakhruddeen <fakhruddeen.mca@gmail.com>
2024-10-29 10:53:15 +0530

    change

--- hrms-create-voucher-service/src/main/java/com/cotodel/hrms/auth/server/controller/HomeController.java
+++ hrms-create-voucher-service/src/main/java/com/cotodel/hrms/auth/server/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.cotodel.hrms.erupistaging.controller;
+package com.cotodel.hrms.auth.server.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
